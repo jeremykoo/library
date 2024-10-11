ac903d63f62d67283e7943d18f43d3c44ce29b33
Jeremy Koo <38170465+jeremykoo@users.noreply.github.com>
2024-10-11 12:26:55 -0700

    refactored library to class

--- javascript.js
+++ javascript.js
@@ -1,11 +1,31 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.info = function() {
+//         let temp = "";
+//         if (this.read == true) {
+//             temp = "already read";
+//         }
+//         else {
+//             temp = "not read yet";
+//         }
+//         return `${this.title} by ${this.author}, ${this.pages} pages, ${temp}`;
+//     }
+// }
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    info() {
         let temp = "";
         if (this.read == true) {
             temp = "already read";
